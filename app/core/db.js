6bbc2145b667412ea22dc05f773c9ea249ca1c5f
Victor Wibisono <vwibisono@dius.com.au>
2016-05-23 09:25:22 +1000

    Log an error if database is not present.

--- app/core/db.js
+++ app/core/db.js
@@ -1,6 +1,12 @@
-var mongoose = require('mongoose');
-var mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/test';
+var mongoose = require('mongoose'),
+    mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/test';
 
-mongoose.connect(mongoUri);
+mongoose.connect(mongoUri, function(err) {
+  console.error(JSON.stringify({
+    time: new Date().toUTCString(),
+    error: err.message,
+    state: 'MongoDB connection error.'
+  }));
+});
 
 module.exports.mongoose = mongoose;
